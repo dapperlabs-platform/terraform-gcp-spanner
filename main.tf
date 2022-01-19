f18f1214b8bbdb179c0ddd290ea3d079ef4a5f1c
Femi Oladele <profemzy@gmail.com>
2022-01-19 18:02:25 +0100

    FIx Instance Ref in Database Creation

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "google_spanner_instance" "default" {
 
 resource "google_spanner_database" "default" {
   for_each = local.databases
-  instance = google_spanner_instance.default
+  instance = google_spanner_instance.default.name
   name     = each.value.name
   deletion_protection = false
 }
