316ae644539653533d7aa52a071fafb35dd07d23
Ben Noyce <ben.noyce@dapperlabs.com>
2022-04-27 21:35:21 -0600

    Undo members versus bindings

--- spanner-db-iam/main.tf
+++ spanner-db-iam/main.tf
@@ -1,7 +1,7 @@
-resource "google_spanner_database_iam_member" "default" {
+resource "google_spanner_database_iam_binding" "default" {
   for_each = var.iams
   database = each.value.database_name
   instance = var.instance
-  member  = each.value.members
+  members  = each.value.members
   role     = each.value.role
 }
