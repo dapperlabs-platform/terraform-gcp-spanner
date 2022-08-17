2e38c7d984875ba5bd55aef62629f2fd376566b7
Uzziah Eyee <uzziah.eyee@dapperlabs.com>
2022-08-17 09:40:00 -0700

    Use IAM Members instead of IAM Bindings.
    - Also make database IAM to explicitly depend on database creation.

--- spanner-db-iam/main.tf
+++ spanner-db-iam/main.tf
@@ -1,7 +1,26 @@
-resource "google_spanner_database_iam_binding" "default" {
-  for_each = var.iams
-  database = each.value.database_name
+#resource "google_spanner_database_iam_binding" "default" {
+#  for_each = var.iams
+#  database = each.value.database_name
+#  instance = var.instance
+#  members  = each.value.members
+#  role     = each.value.role
+#}
+locals {
+  database_role_member = flatten([
+    for k, iamEntry in var.iams :
+    [
+      for membr in iamEntry.members : {
+        role     = iamEntry.role
+        member   = membr
+        database = iamEntry.database_name
+      }
+    ]
+  ])
+}
+resource "google_spanner_database_iam_member" "database" {
+  count    = length(local.database_role_member)
   instance = var.instance
-  members  = each.value.members
-  role     = each.value.role
+  database = local.database_role_member[count.index].database
+  role     = local.database_role_member[count.index].role
+  member   = local.database_role_member[count.index].member
 }
