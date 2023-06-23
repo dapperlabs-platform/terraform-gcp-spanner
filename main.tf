fea5a0c2a025fe542e0bc74a1e530c02af254f51
Ben Noyce <ben@noyce.com>
2023-06-23 13:07:40 -0600

    Update in anticipation of backup module release update

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "db-iam" {
 # Databases Backup
 module "automated-db-backup" {
   count          = (var.backup_enabled == true ? 1 : 0)
-  source         = "github.com/dapperlabs-platform/terraform-gcp-spanner-backup?ref=a_better_backup"
+  source         = "github.com/dapperlabs-platform/terraform-gcp-spanner-backup?ref=v0.2.0"
   database_names = local.database_ids
   instance_name  = google_spanner_instance.default.name
   project_name   = data.google_client_config.this.project
