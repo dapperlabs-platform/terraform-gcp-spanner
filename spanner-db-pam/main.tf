16d3081f36888e2f37861c984a35746107e9635f
michaelgardner87 <michael.gardner@axiomzen.co>
2024-10-09 14:26:41 -0700

    tweaks

--- spanner-db-pam/main.tf
+++ spanner-db-pam/main.tf
@@ -15,7 +15,7 @@ resource "google_privileged_access_manager_entitlement" "entitlement" {
   privileged_access {
     gcp_iam_access {
       role_bindings {
-        role = "roles/spanner.database${var.pam_access[each.key].role}"
+        role = "roles/spanner.database${each.value.role}"
         #condition_expression = "request.time < timestamp(\"2024-04-23T18:30:00.000Z\")"
       }
       resource      = "//cloudresourcemanager.googleapis.com/projects/${var.project_name}"
@@ -27,20 +27,20 @@ resource "google_privileged_access_manager_entitlement" "entitlement" {
   #     "user@example.com",
   #   ]
   #   requester_email_recipients = [
-  #     local.pam_access[each.key].requesters
+  #     each.value.requesters
   #   ]
   # }
 
   dynamic "approval_workflow" {
-    for_each = var.pam_access[each.key].auto_approve == true ? [] : [1]
+    for_each = each.value.auto == true ? [] : [1]
     content {
       manual_approvals {
         require_approver_justification = true
         steps {
           approvals_needed          = 1
-          approver_email_recipients = var.pam_access[each.key].approvers
+          approver_email_recipients = each.value.approvers
           approvers {
-            principals = var.pam_access[each.key].approvers
+            principals = each.value.approvers
           }
         }
       }
